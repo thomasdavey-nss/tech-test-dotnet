880527c6ab7f1c1ddbc815ea9a551c3e2b6525e6
Thomas Davey <temp.github@mail.delor.io>
2026-03-16 20:59:23 +0000

    Refactor PaymentService constructor to use parameterised constructor syntax

--- ClearBank.DeveloperTest/Services/PaymentService.cs
+++ ClearBank.DeveloperTest/Services/PaymentService.cs
@@ -4,30 +4,21 @@ using ClearBank.DeveloperTest.Validators;
 
 namespace ClearBank.DeveloperTest.Services
 {
-    public class PaymentService : IPaymentService
+    public class PaymentService(IDataStore dataStore, IPaymentValidatorFactory paymentValidatorFactory) : IPaymentService
     {
-        private readonly IDataStore _dataStore;
-        private readonly IPaymentValidatorFactory _paymentValidatorFactory;
-
-        public PaymentService(IDataStore dataStore, IPaymentValidatorFactory paymentValidatorFactory)
-        {
-            _dataStore = dataStore;
-            _paymentValidatorFactory = paymentValidatorFactory;
-        }
-        
         public MakePaymentResult MakePayment(MakePaymentRequest request)
         {
-            var account = _dataStore.GetAccount(request.DebtorAccountNumber);
+            var account = dataStore.GetAccount(request.DebtorAccountNumber);
             
             if (account == null) 
                 return MakePaymentResult.Failed();
             
-            var validator = _paymentValidatorFactory.GetValidator(request.PaymentScheme);
+            var validator = paymentValidatorFactory.GetValidator(request.PaymentScheme);
             if (!validator.IsValid(account, request.Amount)) 
                 return MakePaymentResult.Failed();
             
             account.Balance -= request.Amount;
-            _dataStore.UpdateAccount(account);
+            dataStore.UpdateAccount(account);
 
             return MakePaymentResult.Succeeded();
         }
